4b68982f7c4e0a3e7a790a707c0e9883619a35e9
Rajat Ghoshal <rghoshal8989@gmail.com>
2024-01-14 21:59:42 +0530

    Adding operators to the ingestion dag

--- mnt/airflow/dags/solr_ingestion_pipeline.py
+++ mnt/airflow/dags/solr_ingestion_pipeline.py
@@ -1,12 +1,13 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from airflow.operators.email import EmailOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.subdag_operator import SubDagOperator
+# from airflow.executors.sequential_executor import SequentialExecutor
+from connection_index.connection_subdag import factory_subdag
+from datetime import datetime
 
-from datetime import datetime, timedelta
-import csv
-import requests
-import json
+DAG_NAME="solr_ingestion_pipeline"
 
 default_args = {
     "owner": "airflow",
@@ -14,52 +15,46 @@ default_args = {
     "email_on_retry": False,
     "email": "admin@localhost.com",
     "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    'start_date': datetime(2024, 1 ,1), 
 }
 
-def download_rates():
-    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
-    ENDPOINTS = {
-        'USD': 'api_forex_exchange_usd.json',
-        'EUR': 'api_forex_exchange_eur.json'
-    }
-    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
-        reader = csv.DictReader(forex_currencies, delimiter=';')
-        for idx, row in enumerate(reader):
-            base = row['base']
-            with_pairs = row['with_pairs'].split(' ')
-            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
-            outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
-            for pair in with_pairs:
-                outdata['rates'][pair] = indata['rates'][pair]
-            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
-                json.dump(outdata, outfile)
-                outfile.write('\n')
+with DAG(DAG_NAME, schedule_interval="@weekly", 
+         default_args=default_args, catchup=False) as dag:
 
-def _get_message() -> str:
-    return "Hi from forex_data_pipeline"
-
-with DAG("solr_ingestion_pipeline", start_date=datetime(2024, 1 ,1), 
-    schedule_interval="@weekly", default_args=default_args, catchup=False) as dag:
-
-    downloading_rates = PythonOperator(
-        task_id="downloading_rates",
-        python_callable=download_rates
+    register_load = MySqlOperator(
+        task_id = "register_load",
+        sql = '''
+            INSERT INTO load_metadata (llk,status) VALUES ('{{ macros.ds_format(ts_nodash, '%Y%m%dT%H%M%S', '%Y%m%d%H%M%S') }}','IN_PROGRESS')
+        ''',
+        mysql_conn_id = "solr_pipeline_metadata_conn",
     )
 
-    forex_processing = SparkSubmitOperator(
-        task_id="forex_processing",
-        application="/usr/local/airflow/dags/scripts/forex_processing.py",
-        conn_id="spark_conn",
-        verbose=False
+    conn_ingestion = SubDagOperator(
+        task_id='conn_ingestion',
+        subdag=factory_subdag(DAG_NAME, 'conn_ingestion', default_args),
+        # executor=SequentialExecutor()
     )
 
-    send_email_notification = EmailOperator(
-        task_id="send_email_notification",
-        to="airflow_course@yopmail.com",
-        subject="forex_data_pipeline",
-        html_content="<h3>forex_data_pipeline</h3>"
-    )
+    # downloading_rates = PythonOperator(
+    #     task_id="downloading_rates",
+    #     python_callable=download_rates
+    # )
+
+    # forex_processing = SparkSubmitOperator(
+    #     task_id="forex_processing",
+    #     application="/usr/local/airflow/dags/scripts/forex_processing.py",
+    #     conn_id="spark_conn",
+    #     verbose=False
+    # )
+
+    # send_email_notification = EmailOperator(
+    #     task_id="send_email_notification",
+    #     to="airflow_course@yopmail.com",
+    #     subject="forex_data_pipeline",
+    #     html_content="<h3>forex_data_pipeline</h3>"
+    # )
     
-    downloading_rates >> forex_processing 
-    forex_processing >> send_email_notification
+    # downloading_rates >> forex_processing 
+    # forex_processing >> send_email_notification
+
+    register_load >> conn_ingestion
